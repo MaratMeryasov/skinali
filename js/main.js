d4c83da8867351dd5eb9b2819d5c8607a4f527db
Marat_Meryasov <a1@MacBook-Pro-1.local>
2020-09-20 22:54:28 +0300

    Небольшие правки

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(function ($) {
+  $("#phone-2").mask("+7(999)999-9999");
+  $("#user_phone").mask("+7(999)999-9999");
+});
+
+// console.log(document.querySelector('.select'));
+// console.log(document.querySelector('select__dropdown'));
